c172c5da9d47ba1ab017a9cfe63cfd22c9649d38
randomowo <dev@randomowo.dev>
2024-05-30 18:02:24 +0300

    [13] removed godotenv

--- config/app_config.go
+++ config/app_config.go
@@ -3,29 +3,24 @@ package config
 import (
 	"strconv"
 
-	"github.com/joho/godotenv"
 	"github.com/kelseyhightower/envconfig"
 	"go.uber.org/zap"
 )
 
 type AppConfig struct {
-	DataSource            string `required:"true" envconfig:"DATA_SOURCE" default:"postgres"`
-	DbPort                string `required:"true" envconfig:"DATA_SOURCE_PORT" default:"5432"`
-	DbHost                string `required:"true" envconfig:"DATA_SOURCE_HOST" default:"127.0.0.1"`
-	DbName                string `required:"true" envconfig:"DATA_SOURCE_NAME" default:"postgres"`
-	DbUsername            string `required:"true" envconfig:"DATA_SOURCE_USERNAME" default:"postgres"`
-	DbPassword            string `required:"true" envconfig:"DATA_SOURCE_PASSWORD" default:"postgres"`
-	TelegramToken         string `required:"true" envconfig:"TELEGRAM_TOKEN"`
-	TelegramBotDebug      bool   `envconfig:"TELEGRAM_BOT_DEBUG" default:"false"`
-	TelegramUpdateTimeout int    `envconfig:"TELEGRAM_UPDATE_TIMEOUT" default:"60"`
+	DataSource			  string `required:"true" envconfig:"DATA_SOURCE" default:"postgres"`
+	DbPort				  string `required:"true" envconfig:"DATA_SOURCE_PORT" default:"5432"`
+	DbHost				  string `required:"true" envconfig:"DATA_SOURCE_HOST" default:"127.0.0.1"`
+	DbName				  string `required:"true" envconfig:"DATA_SOURCE_NAME" default:"postgres"`
+	DbUsername			  string `required:"true" envconfig:"DATA_SOURCE_USERNAME" default:"postgres"`
+	DbPassword			  string `required:"true" envconfig:"DATA_SOURCE_PASSWORD" default:"postgres"`
+	TelegramToken		  string `required:"true" envconfig:"TELEGRAM_TOKEN"`
+	TelegramBotDebug	  bool	 `envconfig:"TELEGRAM_BOT_DEBUG" default:"false"`
+	TelegramUpdateTimeout int	 `envconfig:"TELEGRAM_UPDATE_TIMEOUT" default:"60"`
 }
 
 // NewConfig Loads environmental variables from .env file and populates AppConfig struct with found values
 func NewConfig(logger *zap.Logger) (*AppConfig, error) {
-	envPath := "config/.env"
-	if err := godotenv.Load(envPath); err != nil {
-		logger.Error("No .env file found for path", zap.String("path", envPath), zap.Error(err))
-	}
 	cfg := AppConfig{}
 	// Process method tries to search environmental variable first by prefix+envconfig
 	// if value isn't found it uses specified envconfig
@@ -41,3 +36,4 @@ func NewConfig(logger *zap.Logger) (*AppConfig, error) {
 		zap.String("Telegram update timeout", strconv.Itoa(cfg.TelegramUpdateTimeout)))
 	return &cfg, nil
 }
+
--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.22.0
 
 require (
 	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
-	github.com/joho/godotenv v1.5.1
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/lib/pq v1.10.9
 	go.uber.org/zap v1.27.0
